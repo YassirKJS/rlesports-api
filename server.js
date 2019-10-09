7051e62a1dde6c34fc1855082166d71349082bcb
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-10-09 13:37:21 +0100

    added CORS

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // users: array of objects
 const db = {
@@ -12,7 +16,6 @@ const db = {
       id: '123',
       username: 'rlesports',
       email: 'rlesports.feed@gmail.com',
-      password: 'banana',
       comments: 0,
       joined: new Date(),
     },
@@ -20,9 +23,15 @@ const db = {
       id: '124',
       username: 'Yassir',
       email: 'khchafyassir12@gmail.com',
-      password: 'muffins',
       comments: 0,
       joined: new Date(),
+    }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'rlesports.feed@gmail.com'
     }
   ]
 }
@@ -33,6 +42,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+  // bcrypt.compareSync("not_bacon", hash);
   if (req.body.email === db.users[0].email && req.body.password === db.users[0].password)  {
     res.json('success');  
   }
@@ -42,7 +52,8 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/signup', (req, res) => {
-  const {username, email, password} = req.body;
+  const {username, email, password} = req.body;   
+  // const hash = bcrypt.hashSync(password, saltRounds); 
   db.users.push(
     {
       id: '125',
@@ -91,52 +102,3 @@ app.listen(3010, () => {
 });
 
 
-
-
-/*
-
-*********Routes***********
-/                 --> res  => this is working
-/signin           --> POST => success/fail
-/signup           --> POST => user
-/profile/:userId  --> GET  => user
-/comments         --> PUT  => user (comments count)
-
-
-POST request : localhost:3010/signin
-  Body/raw
-    {
-      "email": "rlesports.feed@gmail.com",
-      "password": "banana"
-    }
-  
-  => Params
-      "success"
-
-
-POST request: localhost:3010/signup
-  Body/raw
-    {
-      "username": "feed",
-      "email": "rle.feed@gmail.com",
-      "password": "squishy"
-    }
-  
-  => Params
-      {"id":"125","username":"feed","email":"rle.feed@gmail.com","password":"squishy","comments":0,"joined":"2019-10-08T16:18:06.114Z"}
-
-
-GET request:  localhost:3010/profile/123
-  => Params
-      {"id":"123","username":"rlesports","email":"rlesports.feed@gmail.com","password":"banana","comments":4,"joined":"2019-10-08T16:44:56.003Z"}
-
-PUT request:  localhost:3010/comments
-  Body/raw
-    {
-      "id": "123"
-    }
-
-  => Params
-     1 then 2 then 3 etc
-
-*/
